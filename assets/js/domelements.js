48e6976309ae6b3cd3c95771dad231ae717182d6
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2023-02-02 00:04:39 +0100

    initial commit
    all files

---
+++ assets/js/domelements.js
@@ -0,0 +1,25 @@
+
+/*
+brug document.getElementById() til at finde dom elementer der er røde og gule.
+consol logge dem, så du kan se detaljer om dem.
+skriv din kode her
+*/
+
+
+
+
+/*
+brug document.getElementsByClassName() til at finde dom elementer
+og consol logge dem, så du kan se detaljer om dem.
+skriv din kode her
+*/
+
+
+
+
+/*
+find dom elementer med id redFigure og yellowFigure og brug innerHTML til at ændre deres tekst
+skriv din kode her.
+*/
+
+
---
+++ assets/js/events.js
@@ -0,0 +1,62 @@
+
+
+/*opgave 1 plus minus*/
+let myButtonValue = 0;
+const myButtonResult = document.getElementById('buttonResult');
+const myAddButton = document.getElementById('addButton');
+const mySubButton = document.getElementById('subtractButton');
+
+
+//skriv event listners her
+
+
+
+
+
+
+/* opgave 2 dice*/
+const myDiceRes = document.getElementById('diceResult');
+const diceButton = document.getElementById('rollDiceOne');
+
+
+//skriv event listners her
+
+
+
+
+
+
+
+/* opgave 3 key event */
+
+const myLiveText = document.getElementById('myLiveText');
+let myLiveTextResult = document.getElementById('tasteResult');
+
+//skriv event listners her
+
+
+
+
+
+
+
+/* support functions-------------------------------------------------------------------------------*/
+
+/* view code*/
+/* viser data i et DOM element, kræver to parametre: data til at vise og elementet det skal vises i.*/
+function showResult(myData, myElement) {
+    myElement.innerHTML = myData;
+}
+
+
+
+
+/* random function. skal bruge en min værdi og en max værdi.
+returnerer et tilfældigt tal mellem min og max værdierne*/
+
+function getRandomNumber(min, max) {
+    let myMin = Math.ceil(min);
+    let myMax = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
