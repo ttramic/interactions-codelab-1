ba7785fb0cff7021df313b922d34891916b1d066
ttramic <vipmanden@gmail.com>
2023-02-02 20:05:05 +0100

    Update domelements.js

--- assets/js/domelements.js
+++ assets/js/domelements.js
@@ -5,6 +5,11 @@ consol logge dem, så du kan se detaljer om dem.
 skriv din kode her
 */
 
+document.getElementById(redFigure);
+document.getElementById(yellowFigure);
+
+console.log(redFigure, yellowFigure);
+ 
 
 
 
@@ -14,12 +19,18 @@ og consol logge dem, så du kan se detaljer om dem.
 skriv din kode her
 */
 
-
+let myFigure = document.getElementsByClassName('myFigure');
+console.log(myFigure);
 
 
 /*
 find dom elementer med id redFigure og yellowFigure og brug innerHTML til at ændre deres tekst
 skriv din kode her.
 */
+
+document.getElementById("redFigure").innerHTML = "Yellow";
+document.getElementById("yellowFigure").innerHTML = "Red"
+
+
 
 
